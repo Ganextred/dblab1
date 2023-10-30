dde7f45a3a2e5e9842c19969a94e497664a73003
Rapirys <ilyarotan@gmail.com>
2023-10-30 13:14:41 +0200

    More meaningfull trigers

--- Sotored_Procedures.sql
+++ Sotored_Procedures.sql
@@ -1,6 +1,6 @@
-1. Stored Procedure: Insert User
+--1. Stored Procedure: Insert User
 
-```sql
+
 CREATE OR REPLACE PROCEDURE insert_user(IN p_username VARCHAR, IN p_email VARCHAR, IN p_password VARCHAR, IN p_region_id INTEGER)
 LANGUAGE 'plpgsql'
 AS $$
@@ -9,17 +9,17 @@ BEGIN
   VALUES (p_username, p_email, p_password, p_region_id, NOW());
 END;
 $$;
-```
 
-Usage:
 
-```sql
-CALL insert_user('example_username', 'example@example.com', 'example_password', 1);
-```
+--Usage:
+--
+--
+--CALL insert_user('example_username', 'example@example.com', 'example_password', 1);
 
-2. Custom Function: Calculate average game rating
 
-```sql
+--2. Custom Function: Calculate average game rating
+
+
 CREATE FUNCTION avg_game_rating(p_game_id INTEGER)
 RETURNS FLOAT
 AS $$
@@ -32,23 +32,25 @@ BEGIN
   RETURN average_rating;
 END;
 $$ LANGUAGE plpgsql;
-```
 
-Usage:
 
-```sql
-SELECT avg_game_rating(1);
-```
+--Usage:
+--
+--
+--SELECT avg_game_rating(1);
 
-3. Trigger: Update game's `updated_at` and `updated_by` on review creation
 
-```sql
+--3. Trigger: Update game's `updated_at` and `updated_by` on review creation
+
+
 CREATE FUNCTION trigger_update_game_on_review() RETURNS trigger
 AS $$
 BEGIN
-  UPDATE games
-    SET updated_at = NEW.created_at, updated_by = NEW.user_id
-    WHERE id = NEW.game_id;
+  UPDATE games g
+    SET updated_at = NEW.created_at, updated_by = NEW.user_id,
+    rating = (OLD.rating * (SELECT COUNT(*) FROM reviews r WHERE r.id = g.game_id) + NEW.rating)/
+    (SELECT COUNT(*) FROM reviews r WHERE r.id = g.game_id)
+    WHERE g.id = NEW.game_id;
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
@@ -57,11 +59,12 @@ CREATE TRIGGER update_game_on_review
   AFTER INSERT ON reviews
   FOR EACH ROW
   EXECUTE FUNCTION trigger_update_game_on_review();
-```
 
-4. Stored Procedure: Add favorite game for a user
 
-```sql
+
+--4. Stored Procedure: Add favorite game for a user
+
+
 CREATE OR REPLACE PROCEDURE add_favorite_game(IN p_user_id INTEGER, IN p_game_id INTEGER)
 LANGUAGE 'plpgsql'
 AS $$
@@ -70,17 +73,17 @@ BEGIN
   VALUES (p_user_id, p_game_id, NOW());
 END;
 $$;
-```
 
-Usage:
+--
+--Usage:
+--
+--
+--CALL add_favorite_game(1, 1);
 
-```sql
-CALL add_favorite_game(1, 1);
-```
 
-5. Custom Function: Get user's favorite games
+--5. Custom Function: Get user's favorite games
 
-```sql
+
 CREATE FUNCTION get_favorite_games(p_user_id INTEGER)
 RETURNS TABLE (game_id INTEGER)
 AS $$
@@ -91,37 +94,46 @@ BEGIN
   WHERE user_id = p_user_id;
 END;
 $$ LANGUAGE plpgsql;
-```
 
-Usage:
 
-```sql
-SELECT * FROM get_favorite_games(1);
-```
+--Usage:
+--
+--
+--SELECT * FROM get_favorite_games(1);
 
-6. Trigger: Update user's `updated_at` on password change
 
-```sql
-CREATE FUNCTION trigger_update_user_on_password_change() RETURNS trigger
+--6. Trigger: Check game purchasing before adding review
+
+
+CREATE FUNCTION trigger_check_game_purchase_before_review() RETURNS trigger
 AS $$
+DECLARE
+  v_purchased_game BOOLEAN;
 BEGIN
-  IF (OLD.password != NEW.password) THEN
-    NEW.updated_at = NOW();
+  SELECT EXISTS(
+    SELECT 1
+    FROM game_purchases
+    WHERE game_id = NEW.game_id
+    AND user_id = NEW.user_id
+  ) INTO v_purchased_game;
+
+  IF (NOT v_purchased_game) THEN
+    RAISE EXCEPTION 'User cannot review a game without purchasing it';
   END IF;
+
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER update_user_on_password_change
-  BEFORE UPDATE ON users
+CREATE TRIGGER check_game_purchase_before_review
+  BEFORE INSERT ON reviews
   FOR EACH ROW
-  WHEN (OLD.password != NEW.password)
-  EXECUTE FUNCTION trigger_update_user_on_password_change();
-```
+  EXECUTE FUNCTION trigger_check_game_purchase_before_review();
 
-7. Stored Procedure: Update game rating
 
-```sql
+--7. Stored Procedure: Update game rating
+
+
 CREATE OR REPLACE PROCEDURE update_game_rating(p_game_id INTEGER, p_new_rating FLOAT)
 LANGUAGE 'plpgsql'
 AS $$
@@ -131,17 +143,17 @@ BEGIN
     WHERE id = p_game_id;
 END;
 $$;
-```
 
-Usage:
 
-```sql
-CALL update_game_rating(1, 4.5);
-```
+--Usage:
+--
+--
+--CALL update_game_rating(1, 4.5);
 
-8. Custom Function: Get games by genre
 
-```sql
+--8. Custom Function: Get games by genre
+
+
 CREATE FUNCTION get_games_by_genre(p_genre_id INTEGER)
 RETURNS TABLE (game_id INTEGER)
 AS $$
@@ -152,36 +164,47 @@ BEGIN
   WHERE genre_id = p_genre_id;
 END;
 $$ LANGUAGE plpgsql;
-```
 
-Usage:
 
-```sql
-SELECT * FROM get_games_by_genre(1);
-```
+--Usage:
+--
+--
+--SELECT * FROM get_games_by_genre(1);
 
-9. Trigger: Update game's `updated_at` and `updated_by` on game_purchase
 
-```sql
-CREATE FUNCTION trigger_update_game_on_purchase() RETURNS trigger
+--9. Trigger Create game_purchasing for user only if game released in user region
+
+
+CREATE FUNCTION trigger_check_game_release_region() RETURNS trigger
 AS $$
+DECLARE
+  v_regional_release_date DATE;
 BEGIN
-  UPDATE games
-    SET updated_at = NEW.updated_at, updated_by = NEW.user_id
-    WHERE id = NEW.game_id;
+  SELECT release_date
+  INTO v_regional_release_date
+  FROM game_publishing
+  WHERE game_id = NEW.game_id
+  AND region_id = (SELECT region_id FROM users WHERE id = NEW.user_id);
+
+  IF (v_regional_release_date IS NULL) THEN
+    RAISE EXCEPTION 'This game has not been released in your region';
+  ELSIF (v_regional_release_date > CURRENT_DATE) THEN
+    RAISE EXCEPTION 'This game has not been released in your region yet';
+  END IF;
+
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER update_game_on_purchase
-  AFTER INSERT ON game_purchases
+CREATE TRIGGER check_game_release_region
+  BEFORE INSERT ON game_purchases
   FOR EACH ROW
-  EXECUTE FUNCTION trigger_update_game_on_purchase();
-```
+  EXECUTE FUNCTION trigger_check_game_release_region();
 
-10. Stored Procedure: Update developer
 
-```sql
+--10. Stored Procedure: Update developer
+
+
 CREATE OR REPLACE PROCEDURE update_developer(IN p_id INTEGER, IN p_name VARCHAR, IN p_founded_date DATE, IN p_website VARCHAR, IN p_country VARCHAR, IN p_updated_by INTEGER)
 LANGUAGE 'plpgsql'
 AS $$
@@ -191,10 +214,9 @@ BEGIN
     WHERE id = p_id;
 END;
 $$;
-```
 
-Usage:
 
-```sql
-CALL update_developer(1, 'Updated Developer', '2000-01-01', 'https://updated-dev.com', 'Updated Country', 1);
-```
+--Usage:
+--
+--
+--CALL update_developer(1, 'Updated Developer', '2000-01-01', 'https://updated-dev.com', 'Updated Country', 1);
